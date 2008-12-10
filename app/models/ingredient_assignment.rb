c14b8d89eb207c132746548490930d1249d15322
Jorge Villatoro <programmerjorge@gmail.com>
2008-12-10 17:00:45 -0600

    Commented out validation code

--- app/models/ingredient_assignment.rb
+++ app/models/ingredient_assignment.rb
@@ -7,21 +7,21 @@ class IngredientAssignment
   property :quantity, Float, :nullable => false
   property :units, String, :nullable => false
   
-  validates_with_block :quantity do
-    if quantity && quantity > 0.0
-      true
-    else
-      [false, "Quantity must be greater than 0"]
-    end
-  end
+#  validates_with_block :quantity do
+#    if quantity && quantity > 0.0
+#      true
+#    else
+#      [false, "Quantity must be greater than 0"]
+#    end
+#  end
   
-  validates_with_block :type do
-    if descendants.include?(Object.full_const_get(type || ""))
-      true
-    else
-      [false, "Invalid type specified"]
-    end
-  end
+#  validates_with_block :type do
+#    if descendants.include?(Object.full_const_get(type || ""))
+#      true
+#    else
+#      [false, "Invalid type specified"]
+#    end
+#  end
 
   belongs_to :recipe
 end
