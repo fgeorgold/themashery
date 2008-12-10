7605b4b2fb54f5b119750a1153b62f53e1d4fecd
Jorge Villatoro <programmerjorge@gmail.com>
2008-12-09 22:03:06 -0600

    Added helpers for optionally displaying resource links

--- app/helpers/global_helpers.rb
+++ app/helpers/global_helpers.rb
@@ -6,6 +6,37 @@ module Merb
         link_string + " |"
       end
     end
- 
+    
+    def authz_new_link(target, text = "New")
+      klass = Object.full_const_get(target.to_s.singular.camel_case)
+      if session.user.authorized?(:create, :target => klass)
+        link_to text, resource(target, :new)
+      end
+    end
+    
+    def authz_edit_link(target, text = "Edit")
+      if session.user.authorized?(:update, :target => target)
+        link_to text, resource(target, :edit)
+      end
+    end
+    
+    def authz_delete_link(target, text = "Delete")
+      if session.user.authorized?(:delete, :target => target)
+        delete_button(target, text)
+      end
+    end
+    
+    def authz_show_link(target, text = "Show")
+      if target.is_a?(Symbol)
+        auth_target = Object.full_const_get(target.to_s.singular.camel_case)
+      else
+        auth_target = target
+      end
+      
+      if session.user.authorized?(:read, :target => auth_target)
+        link_to text, resource(target)
+      end
+    end
+    
   end
 end
