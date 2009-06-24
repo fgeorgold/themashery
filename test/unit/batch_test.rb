c7a2960bd6f5a2d553b8a4df6c48170b56e2e8b3
Jorge Villatoro <jorge@Bender.local>
2009-06-24 14:01:18 -0500

    Updated the fixtures, added failing unit tests for needed validations

--- test/unit/batch_test.rb
+++ test/unit/batch_test.rb
@@ -2,7 +2,19 @@ require 'test_helper'
 
 class BatchTest < ActiveSupport::TestCase
   # Replace this with your real tests.
-  test "the truth" do
-    assert true
+  test "must have recipe" do
+    assert !batches(:no_recipe_id).save, "Batch saved without a recipe"
+  end
+  
+  test "must have a brew date" do
+    assert !batches(:no_brewed_on).save, "Batch saved without a brew date"
+  end
+  
+  test "brew date must be today or earlier" do
+    assert !batches(:brewed_tomorrow).save, "Batch saved with a brew date later than today"
+  end
+  
+  test "valid batch fixture should save" do
+    assert batches(:valid).save, "Valid batch fixture did not save"
   end
 end
