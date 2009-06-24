dba2bdcb2396bfd6090760f90225d7aea9c9807d
Jorge Villatoro <jorge@Bender.local>
2009-06-24 14:35:10 -0500

    added prettier error message to the date validation

--- app/models/batch.rb
+++ app/models/batch.rb
@@ -1,4 +1,4 @@
 class Batch < ActiveRecord::Base
   validates_presence_of :recipe_id
-  validates_inclusion_of :brewed_on, :in => (Date.new..Date.today)
+  validates_inclusion_of :brewed_on, :in => (Date.new..Date.today), :message => "date must be today or earlier"
 end
