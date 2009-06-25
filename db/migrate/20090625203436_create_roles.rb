f5a95c74448ce326fc186ece1bd0700df98fa663
Jorge Villatoro <jorge@Bender.local>
2009-06-25 15:47:39 -0500

    Added role_requirement plugin

---
+++ db/migrate/20090625203436_create_roles.rb
@@ -0,0 +1,22 @@
+class CreateRoles < ActiveRecord::Migration
+  def self.up
+    create_table "roles" do |t|
+      t.string :name
+    end
+    
+    # generate the join table
+    create_table "roles_users", :id => false do |t|
+      t.integer "role_id", "user_id"
+    end
+    add_index "roles_users", "role_id"
+    add_index "roles_users", "user_id"
+    
+    Role.create :name => "admin"
+    Role.create :name => "user"
+  end
+
+  def self.down
+    drop_table "roles"
+    drop_table "roles_users"
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -9,7 +9,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20090625172324) do
+ActiveRecord::Schema.define(:version => 20090625203436) do
 
   create_table "batches", :force => true do |t|
     t.integer  "recipe_id"
@@ -28,6 +28,18 @@ ActiveRecord::Schema.define(:version => 20090625172324) do
     t.datetime "updated_at"
     t.integer  "user_id"
   end
+
+  create_table "roles", :force => true do |t|
+    t.string "name"
+  end
+
+  create_table "roles_users", :id => false, :force => true do |t|
+    t.integer "role_id"
+    t.integer "user_id"
+  end
+
+  add_index "roles_users", ["role_id"], :name => "index_roles_users_on_role_id"
+  add_index "roles_users", ["user_id"], :name => "index_roles_users_on_user_id"
 
   create_table "users", :force => true do |t|
     t.string   "email",                             :null => false
